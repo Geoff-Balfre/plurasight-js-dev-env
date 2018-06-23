84f360c206d5ea38ece3459140302a89920d77cf
Geoff-Balfre <geoff@balfre.com>
2018-06-23 19:05:04 +0100

    post bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-const local   = 'http://localhost:';
-const port    = 3001;
-const app     = express();
+const local    = 'http://localhost:';
+const port     = 3001;
+const app      = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function (req,res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
