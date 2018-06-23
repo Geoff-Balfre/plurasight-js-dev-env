c7493d500ec2b6aca8bc3caa6319114db31ee085
Geoff-Balfre <geoff@balfre.com>
2018-06-23 12:13:38 +0100

    latest

---
+++ buildScripts/postMessage.js
@@ -0,0 +1,4 @@
+
+import chalk from 'chalk';
+
+console.log(chalk.green('POST: app now in dev mode...')); // eslint-disable-line no-console
---
+++ buildScripts/preMessage.js
@@ -0,0 +1,5 @@
+//var chalk = require('chalk');
+
+import chalk from 'chalk';
+
+console.log(chalk.red('PRE: Starting app in dev mode...')); // eslint-disable-line no-console
